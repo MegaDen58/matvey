d963372f9d9300caa42be929631a142c8e85041e
cemetryghost <matvey.marusik@mail.ru>
2022-12-15 14:11:45 +0300

    commit

---
+++ src/main/java/com/example/auto/AuthorizationView.java
@@ -0,0 +1,24 @@
+package com.example.auto;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+
+public class AuthorizationView {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+
+}
---
+++ src/main/java/com/example/auto/HelloController.java
@@ -0,0 +1,32 @@
+package com.example.auto;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button adminButton;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    void adminEnter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userEnter(ActionEvent event) {
+
+    }
+}
